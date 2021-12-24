8af236c8f10764342557573257c9e3e521adc330
Peter Vila <peterdvila@gmail.com>
2021-12-23 19:46:59 -0800

    Added filter controlled component with .scss. Component resides in ExpenseList

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,22 +1,36 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.scss";
+import ExpenseFilter from "./ExpenseFilter"
 import Card from '../UI/Card';
 
 const ExpenseList = (props) => {
+  const [filteredYear, setFilteredYear] = useState(2021);
+
+  const filterChangeHandler = selectedYear => {
+    console.log('ExpenseList.js');
+    setFilteredYear(selectedYear);
+  }
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense, index) => {
-        return (
-          <ExpenseItem
-            key={index}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-    </Card>
-  )
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          filterChangeHandler={filterChangeHandler}
+          selected={filteredYear}
+        />
+        {props.expenses.map((expense, index) => {
+          return (
+            <ExpenseItem
+              key={index}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
+  );
 }
 
 export default ExpenseList;
